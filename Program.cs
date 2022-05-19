9c928ebbf757a93ce3f2c9bcf4f7a19cf5116f25
lemuelpaz <lemuelpazcontato@hotmail.com>
2022-05-18 22:34:46 -0300

    Convertendo-String-Int

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Convertendo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Dados = "123";
+            int Converti = Int32.Parse(Dados);
+            System.Console.WriteLine(Converti);
+        }
+    }
+}
